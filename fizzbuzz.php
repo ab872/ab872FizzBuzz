378f6acb6250bd9eaa70b1ddf72d4d129e4ac4c6
A. Bright <ab8720843@gmail.com>
2024-08-14 16:07:58 +0800

    fizzbuzz.php   adjust layout for cross-browser compatibility

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -411,7 +411,7 @@ switch($screenWidth) {
 				$containerIntroRows = "100vh";
 				$containerCodePHPRows = "80vh";
         			$containerV3LicenseRows = "160vh";
-				$containerReferenceRows = "1140vh";
+				$containerReferenceRows = "1180vh";
          		} 
                         
          		if($isFirefox) {
